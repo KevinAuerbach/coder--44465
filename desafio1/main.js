517d2a849cc2d9a859611778b105c5b06802b4ef
Ramiro <ramifazio@gmail.com>
2022-09-05 11:09:02 -0300

    Sugerencias!

--- desafio1/main.js
+++ desafio1/main.js
@@ -1,5 +1,9 @@
-let entrada =parseInt(prompt("Ingrese un numero"))
+let entrada = parseInt(prompt("Ingrese un numero"));
 
-for(let i = 0; i < entrada; i++){
-    console.log("Hola")
+let suma = 0; //Declare un contador en 0
+for (let i = 0; i < entrada; i++) {
+  console.log("Hola");
+  suma++;//Cada vez que pasa por aca le suma 1
 }
+
+alert(`Tarde ${suma} veces en llegar a tu numero :D`);//Interpolo Strings con Variables usando `BackTicks`
